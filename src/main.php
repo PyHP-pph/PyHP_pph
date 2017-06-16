027f9ac984f93ae2f9a5ff2ee37cbf1bd0f1c6d7
Alex Martin <alexbuzzbee@gmail.com>
2017-06-16 18:21:01 -0400

    Fix semicolons in CIC `main.php`

--- src/main.php
+++ src/main.php
@@ -1,8 +1,8 @@
 <?php
 // Main PHP file for PyHP++# compiler-interpreter-compiler.
 
-echo "The spec isn't even remotely ready yet, and you expect the compiler-interpreter-compiler to work?\n"
+echo "The spec isn't even remotely ready yet, and you expect the compiler-interpreter-compiler to work?\n";
 
-echo "No. Just no.\n"
+echo "No. Just no.\n";
 
 ?>
